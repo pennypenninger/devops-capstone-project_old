af8be8d459d4aa7d2eac59ae682a02f796ae2ddb
pennypenninger <144658293+pennypenninger@users.noreply.github.com>
2024-05-21 14:30:10 +0200

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -50,7 +50,7 @@ class TestAccountService(TestCase):
         """Runs once after each test case"""
         db.session.remove()
 
-  def test_get_account(self):
+    def test_get_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.get(
@@ -59,6 +59,11 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
+
+   def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
   
     ######################################################################
     #  H E L P E R   M E T H O D S
